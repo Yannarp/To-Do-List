6d6900bca47028f1565e3268dc04ca4db67ca23a
Yanna Pecanha <yannarp@hotmail.com>
2022-04-08 21:15:20 -0300

    First commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Adding validation for empty input field
 document.querySelector('#add').onclick = function () {
     if (document.querySelector('#new-task input').value.length == 0) {
-      alert('Favor adicionar nova tarefa')
+      alert('Adicionar nova tarefa')
     }
   
     //Adding new task
@@ -44,26 +44,10 @@ document.querySelector('#add').onclick = function () {
     changeText()
   }
   
-  function changeClasses() {
-    button.classList.toggle('light-mode')
-    body.classList.toggle('light-mode')
-    newTask.classList.toggle('light-mode')
-    tasks.classList.toggle('light-mode')
-    logo.classList.toggle('light-mode')
-  }
-  
-  function changeText() {
-    if (body.classList.contains('light-mode')) {
-      button.innerHTML = 'Dark Mode'
-      return
-    }
-  
-    button.innerHTML = 'Light Mode'
-  }
+    
   
   const logo = document.getElementById('logo')
   const tasks = document.getElementById('tasks')
   const newTask = document.getElementById('new-task')
-  const button = document.getElementById('mode-selector')
   const body = document.getElementsByTagName('body')[0]
   button.addEventListener('click', changeMode)
